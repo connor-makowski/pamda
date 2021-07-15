01992743801b3cc42bade1a2e65b11a072376e70
Connor Makowski <connorman528@gmail.com>
2021-07-15 11:55:00 -0400

    v0.0.8 Add exceptions to not curry utils with kwargs.

--- pamda/pamda.py
+++ pamda/pamda.py
@@ -2,8 +2,13 @@ from pamda.pamda_core import pamda_core
 
 class pamda_core_curried:
     def __init__(self, pamda, *args, **kwargs):
+        no_curry=['read_csv','read_json','write_csv','write_json', 'warnt', 'vprint', 'exception']
         for method in pamda.getMethods(pamda):
-            setattr(self, method, pamda.curry(getattr(pamda, method)))
+            if method in no_curry:
+                setattr(self, method, getattr(pamda, method))
+            else:
+                setattr(self, method, pamda.curry(getattr(pamda, method)))
+
 
 pamda_uncurried=pamda_core()
 pamda=pamda_core_curried(pamda=pamda_uncurried)
