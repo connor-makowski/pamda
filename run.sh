99cfe0dc83f7923d3bae389460acd4a71c9b9c2a
Connor Makowski <connorman528@gmail.com>
2025-03-14 14:41:41 -0400

    Update docker container name

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-docker build . --tag "type_enforced" --quiet > /dev/null
+docker build . --tag "pamda" --quiet > /dev/null
 # if an arg was passed: use it as an entrypoint
 if [ -z "$1" ]; then
     docker run -it --rm \
         --volume "$(pwd):/app" \
-        "type_enforced"
+        "pamda"
 else
     docker run -it --rm \
         --volume "$(pwd):/app" \
         --entrypoint "/app/utils/$1.sh" \
-        "type_enforced"
+        "pamda"
 fi
