888af6f2c00c94eecb6afa78ca20efb7f3fa7e62
Connor Makowski <connorman528@gmail.com>
2025-08-14 16:56:37 -0400

    Add merge deep test

--- test/time_tests.py
+++ test/time_tests.py
@@ -1,5 +1,6 @@
 from pamda import pamda
 from pamda.pamda_timer import pamda_timer
+from pamda.pamda_fast import __assocPath__
 
 print("\n===============\n1M Scale Time Tests:\n===============")
 
@@ -28,13 +29,27 @@ data = (
     ]
 )
 
+# Make a simple 10 tier nested structure for testing mergeDeep with random keys in each layer
+from random import randint
+
+data = {}
+for i in range(1000000):
+    path = [f'level{randint(1, 1000)}' for _ in range(10)]
+    __assocPath__(path, randint(1, 1000), data)
+
+data_merge = {}
+for i in range(1000000):
+    path = [f'level{randint(1, 1000)}' for _ in range(10)]
+    __assocPath__(path, randint(1, 1000), data_merge)
+
 for function, args in [
-    (pamda.groupBy, [lambda x: str(x["color"] + x["shape"]), data]),
-    (pamda.groupKeys, [["color", "size"], data]),
-    (pamda.nest, [["color", "size"], "size", data]),
-    (pamda.nestItem, [["color", "size"], data]),
-    (pamda.pluck, ["color", data]),
-    (pamda.pluckIf, [lambda x: x["color"] == "red", ["color"], data]),
+    # (pamda.groupBy, [lambda x: str(x["color"] + x["shape"]), data]),
+    # (pamda.groupKeys, [["color", "size"], data]),
+    # (pamda.nest, [["color", "size"], "size", data]),
+    # (pamda.nestItem, [["color", "size"], data]),
+    # (pamda.pluck, ["color", data]),
+    # (pamda.pluckIf, [lambda x: x["color"] == "red", ["color"], data]),
+    (pamda.mergeDeep, [data_merge, data]),
 ]:
     pamda_timer(function, iterations=3, print_time_stats=True).get_time_stats(*args)
 
